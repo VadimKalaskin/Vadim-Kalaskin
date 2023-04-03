d594347946e37ad579bad5b4b54d069b497f5cb5
VadimKalaskin <120710280+VadimKalaskin@users.noreply.github.com>
2023-04-03 15:27:22 +0300

    Доработки

--- sneaker-shop/src/App.js
+++ sneaker-shop/src/App.js
@@ -5,6 +5,12 @@ import user from './img/user.svg'
 import addToCart from './img/addToCart.svg'
 import addedToCart from './img/addedToCart.svg'
 import nike1 from './img/Nike Blazer Mid Suede.jpg'
+import search from './img/Search.svg'
+import addToHeart from './img/addToHeart.svg'
+import addedToHeart from './img/addedToHeart.svg'
+import close from './img/close.svg'
+
+
 
 
 
@@ -12,6 +18,29 @@ import nike1 from './img/Nike Blazer Mid Suede.jpg'
 function App() {
   return (
     <div className="container mt-5">
+
+      <div className="overlay">
+        <div className="drawer">
+          <h2>Корзина</h2>
+          <div className="cart-item">
+            <img className="cart-item-img" src={nike1} />
+            <div>
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <p>12 999 руб.</p>
+              </div>
+              <img className="close-btn" src={close} alt="close" />
+          </div>
+          <div className="cart-item">
+            <img className="cart-item-img" src={nike1} />
+            <div>
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <p>12 999 руб.</p>
+              </div>
+              <img className="close-btn" src={close} alt="close" />
+          </div>
+        </div>
+      </div>
+
       <header className="header d-flex justify-content-between align-items-center">
         <div className="header_col d-flex justify-content-center align-items-center">
           <div className="header_logo me-3"><img src={logo} alt="" /></div>
@@ -22,21 +51,28 @@ function App() {
         </div>
         <div className="header_col d-flex justify-content-center align-items-center">
           <div className="header_cart">
-            <img src={cart} alt="" />
+            <img src={cart} alt="cart" />
           </div>
           <div className="header_price"><span>12500</span> руб.</div>
           <div className="header_heart">
-            <img src={heart} alt="" />
+            <img src={heart} alt="Heart" />
           </div>
           <div className="header_user">
-            <img src={user} alt="" />
+            <img src={user} alt="user" />
           </div>
         </div>
       </header>
       <div className="content">
-        <h2>Все кроссовки</h2>
+        <div className="d-flex justify-content-between align-items-center">
+          <h2>Все кроссовки</h2>
+          <div className="search-block">
+            <img src={search} alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
         <div className="row">
           <div className="item_card col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            <img className="like" src={addToHeart} alt="Unliked" />
             <img className="card_img" src={nike1} alt="" />
             <h4 className="card_title">Мужские Кроссовки Nike Blazer Mid Suede</h4>
             <div className="price_wrapper"><div className="card_price">
@@ -47,6 +83,7 @@ function App() {
             {/* <button className="item_addToHeart btn"><img src={addedToCart} alt="" /></button> */}
           </div>
           <div className="item_card col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            <img className="like" src={addToHeart} alt="Unliked" />
             <img className="card_img" src={nike1} alt="" />
             <h4 className="card_title">Мужские Кроссовки Nike Blazer Mid Suede</h4>
             <div className="price_wrapper"><div className="card_price">
@@ -57,6 +94,7 @@ function App() {
             {/* <button className="item_addToHeart btn"><img src={addedToCart} alt="" /></button> */}
           </div>
           <div className="item_card col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            <img className="like" src={addToHeart} alt="Unliked" />
             <img className="card_img" src={nike1} alt="" />
             <h4 className="card_title">Мужские Кроссовки Nike Blazer Mid Suede</h4>
             <div className="price_wrapper"><div className="card_price">
@@ -67,6 +105,7 @@ function App() {
             {/* <button className="item_addToHeart btn"><img src={addedToCart} alt="" /></button> */}
           </div>
           <div className="item_card col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            <img className="like" src={addToHeart} alt="Unliked" />
             <img className="card_img" src={nike1} alt="" />
             <h4 className="card_title">Мужские Кроссовки Nike Blazer Mid Suede</h4>
             <div className="price_wrapper"><div className="card_price">
@@ -76,26 +115,8 @@ function App() {
             
             {/* <button className="item_addToHeart btn"><img src={addedToCart} alt="" /></button> */}
           </div>
-          <div className="item_card col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <img className="card_img" src={nike1} alt="" />
-            <h4 className="card_title">Мужские Кроссовки Nike Blazer Mid Suede</h4>
-            <div className="price_wrapper"><div className="card_price">
-              <span>ЦЕНА:</span><span>12 999 руб.</span>
-            </div>
-            <button className="item_addToCart btn"><img src={addToCart} alt="Добавить в корзину" /></button></div>
-            
-            {/* <button className="item_addToHeart btn"><img src={addedToCart} alt="" /></button> */}
-          </div>
-          <div className="item_card col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <img className="card_img" src={nike1} alt="" />
-            <h4 className="card_title">Мужские Кроссовки Nike Blazer Mid Suede</h4>
-            <div className="price_wrapper"><div className="card_price">
-              <span>ЦЕНА:</span><span>12 999 руб.</span>
-            </div>
-            <button className="item_addToCart btn"><img src={addToCart} alt="Добавить в корзину" /></button></div>
-            
-            {/* <button className="item_addToHeart btn"><img src={addedToCart} alt="" /></button> */}
-          </div>
+
+
         </div>
       </div>
     </div>
