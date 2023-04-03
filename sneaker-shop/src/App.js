dba395537099d8a70d624b1aa47c2b34f713d4c6
Vadim Kalaskin <kalaskin1@icloud.com>
2023-04-03 22:54:00 +0300

    Добавил пропсы

--- sneaker-shop/src/App.js
+++ sneaker-shop/src/App.js
@@ -1,21 +1,29 @@
-
-import search from './img/Search.svg'
+import search from './img/Search.svg';
 //components
 import Card from './components/Card';
 import Cart from './components/Cart';
 import Header from './components/Header';
 
-
-
-
-
-
+const arr = [
+  {
+    name: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 12990,
+    imageUrl: '/images/image5.png',
+  },
+  { name: 'Мужские Кроссовки Nike Air Max 270', price: 15590, imageUrl: '/images/image5-1.png' },
+  {
+    name: 'Мужские Кроссовки Nike Blazer Mid Beige',
+    price: 11990,
+    imageUrl: '/images/image5-2.png',
+  },
+  { name: 'Кроссовки Puma X Aka Boku Future Rider', price: 6890, imageUrl: '/images/image5-3.png' },
+];
 
 function App() {
   return (
     <div className="container mt-5">
       <Header />
-      <Cart/>
+      <Cart />
       <div className="content">
         <div className="d-flex justify-content-between align-items-center">
           <h2>Все кроссовки</h2>
@@ -25,12 +33,9 @@ function App() {
           </div>
         </div>
         <div className="row">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((obj) => (
+            <Card name={obj.name} price={obj.price} imageUrl={obj.imageUrl} />
+          ))}
         </div>
       </div>
     </div>
--- sneaker-shop/src/components/Card.js
+++ sneaker-shop/src/components/Card.js
@@ -1,25 +1,26 @@
-import addToHeart from '../img/addToHeart.svg'
-import addedToHeart from '../img/addedToHeart.svg'
+import addToHeart from '../img/addToHeart.svg';
+import addedToHeart from '../img/addedToHeart.svg';
 
-import addToCart from '../img/addToCart.svg'
-import addedToCart from '../img/addedToCart.svg'
+import addToCart from '../img/addToCart.svg';
+import addedToCart from '../img/addedToCart.svg';
 
-import nike1 from '../img/Nike Blazer Mid Suede.jpg'
-
- 
- 
- function Card() {
-    return(
+function Card(props) {
+  return (
     <div className="item_card col-lg-3 col-md-4 col-sm-6 col-xs-12">
-    <img className="like" src={addToHeart} alt="Unliked" />
-    <img className="card_img" src={nike1} alt="" />
-    <h4 className="card_title">Мужские Кроссовки Nike Blazer Mid Suede</h4>
-    <div className="price_wrapper"><div className="card_price">
-      <span>ЦЕНА:</span><span>12 999 руб.</span>
+      <img className="like" src={addToHeart} alt="Unliked" />
+      <img className="card_img" src={props.imageUrl} alt={props.name} />
+      <h4 className="card_title">{props.name}</h4>
+      <div className="price_wrapper">
+        <div className="card_price">
+          <span>ЦЕНА:</span>
+          <span>{props.price} руб.</span>
+        </div>
+        <button className="item_addToCart btn">
+          <img src={addToCart} alt="Добавить в корзину" />
+        </button>
+      </div>
     </div>
-    <button className="item_addToCart btn"><img src={addToCart} alt="Добавить в корзину" /></button></div>
-  </div>)
+  );
+}
 
- }
-
- export default Card;
+export default Card;
