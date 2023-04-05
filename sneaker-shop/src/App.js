51f2dcd6c667c98bd950d2736316e1bf4985c172
VadimKalaskin <120710280+VadimKalaskin@users.noreply.github.com>
2023-04-05 10:58:52 +0300

    key для повтор. элементов и реализация контроллируемого инпута

--- sneaker-shop/src/App.js
+++ sneaker-shop/src/App.js
@@ -1,4 +1,6 @@
 import search from './img/Search.svg';
+import close from './img/close.svg';
+
 //components
 import React from 'react';
 import Card from './components/Card';
@@ -11,6 +13,11 @@ function App() {
   const [cartOpened, setCartOpened] = React.useState(false);
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const onChangeSearchInput = (e) => {
+    setSearchValue(e.target.value);
+  }
 
   React.useEffect(() => {
     fetch('https://642c3132208dfe25472a75cf.mockapi.io/items')
@@ -37,15 +44,17 @@ function App() {
       )}
       <div className="content">
         <div className="d-flex justify-content-between align-items-center">
-          <h2>Все кроссовки</h2>
+          <h2> {searchValue ? `Поиск по запросу: ${searchValue}` : "Все кроссовки"}</h2>
           <div className="search-block">
             <img src={search} alt="Search" />
-            <input placeholder="Поиск..." />
+            <input placeholder="Поиск..." onChange={onChangeSearchInput} value={searchValue}  />
+            {searchValue && <img src={close} alt="Close" className='close' onClick={() => {setSearchValue('')}} />}
           </div>
         </div>
         <div className="row">
+          
           {items.map((obj) => (
-            <Card
+            <Card key={obj.imageUrl}
               name={obj.name}
               price={obj.price}
               imageUrl={obj.imageUrl}
