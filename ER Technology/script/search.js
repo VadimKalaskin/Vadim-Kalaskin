27fd8557e10861ab6ee7d91b2876055560e38351
VadimKalaskin <120710280+VadimKalaskin@users.noreply.github.com>
2023-04-12 09:40:19 +0300

    11

--- ER Technology/script/search.js
+++ ER Technology/script/search.js
@@ -1,29 +1,63 @@
+//Иконка и инпут
+
+const svg = document.querySelector('.header_search svg');
+const searchInput = document.querySelector(".search-input");
+const headerLang = document.querySelector('.header_lang');
+
+svg.addEventListener('click', () => {
+  svg.classList.toggle('active');
+  searchInput.classList.toggle('active');
+  headerLang.classList.toggle('displaynone');
+});
+
+//Иконка и инпут
+// Логика поиска и поведение блоков
+
 var allItems = document.querySelectorAll(".main-item");
 var originalTexts = [];
 
 for (var i = 0; i < allItems.length; i++) {
-  originalTexts.push(allItems[i].querySelector("p").textContent);
+  originalTexts.push([]);
+  var pElems = allItems[i].querySelectorAll("p");
+  for (var j = 0; j < pElems.length; j++) {
+    originalTexts[i].push(pElems[j].textContent);
+  }
 }
 
-var searchInput = document.querySelector(".search-input");
 searchInput.addEventListener("input", function(event) {
   var searchValue = event.target.value.trim().toLowerCase();
 
   if (event.target.value === "") {
     for (var i = 0; i < allItems.length; i++) {
-      allItems[i].style.display = "flex";
-      allItems[i].querySelector("p").innerHTML = originalTexts[i];
+      allItems[i].style.display = "block";
+      var pElems = allItems[i].querySelectorAll("p");
+      for (var j = 0; j < pElems.length; j++) {
+        pElems[j].innerHTML = originalTexts[i][j];
+      }
     }
   } else {
     for (var i = 0; i < allItems.length; i++) {
-      var itemText = allItems[i].querySelector("p").textContent.toLowerCase();
-      if (itemText.indexOf(searchValue) === -1) {
+      var itemTexts = [];
+      var pElems = allItems[i].querySelectorAll("p");
+      for (var j = 0; j < pElems.length; j++) {
+        itemTexts.push(pElems[j].textContent.toLowerCase());
+      }
+      if (itemTexts.join("").indexOf(searchValue) === -1) {
         allItems[i].style.display = "none";
       } else {
-        var regex = new RegExp(searchValue, "gi");
-        allItems[i].querySelector("p").innerHTML = itemText.replace(regex, "<span style='color: #3694E7;'>" + searchValue + "</span>");
-        allItems[i].style.display = "flex";
+        for (var j = 0; j < pElems.length; j++) {
+          var itemText = pElems[j].textContent.toLowerCase();
+          var regex = new RegExp(searchValue, "gi");
+          pElems[j].innerHTML = itemText.replace(regex, "<span style='color: #3694E7;'>" + searchValue + "</span>");
+        }
+        allItems[i].style.display = "block";
+        if(!allItems[i].querySelector('.plusminus').classList.contains("active")) {
+          allItems[i].querySelector('button').click();
+        }
+        
       }
     }
   }
 });
+
+// Логика поиска и поведение блоков
