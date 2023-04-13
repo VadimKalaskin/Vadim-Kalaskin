a46f828128fd3b1f402e0f2d852ea24b6eea37aa
Vadim Kalaskin <kalaskin1@icloud.com>
2023-04-13 21:57:57 +0300

    bug fix

--- ER Technology/script/accordion.js
+++ ER Technology/script/accordion.js
@@ -1,30 +1,31 @@
 const mainItems = document.querySelectorAll('.main-item');
+const buttonPrincipes = document.querySelector('.principesBtn');
+const principes = buttonPrincipes.closest('.container-fluid');
+const moreInfo = document.querySelectorAll('.principes .more-info');
 
-mainItems.forEach(mainItem => {
+mainItems.forEach((mainItem) => {
   const buttons = mainItem.querySelectorAll('button');
-  buttons.forEach(button => {
+  buttons.forEach((button) => {
     button.addEventListener('click', () => {
       button.querySelector('.plusminus').classList.toggle('active');
       const moreInfo = mainItem.querySelectorAll('.more-info');
-      moreInfo.forEach(block => {
+      moreInfo.forEach((block) => {
         block.classList.toggle('active');
-        block.style.maxHeight = block.classList.contains('active') ? `${block.scrollHeight}px` : '0';
+        block.style.maxHeight = block.classList.contains('active')
+          ? `${block.scrollHeight}px`
+          : '0';
       });
-    })
+    });
   });
 });
-
-const buttonPrincipes = document.querySelector('.principesBtn');
-const principes = buttonPrincipes.closest('.container-fluid');
-const moreInfo = principes.querySelectorAll('.more-info');
 
 buttonPrincipes.addEventListener('click', () => {
   buttonPrincipes.classList.toggle('active');
   principes.classList.toggle('active');
 
-  moreInfo.forEach(block => {
+  moreInfo.forEach((block) => {
+    console.log(moreInfo);
     block.classList.toggle('active');
     block.style.maxHeight = block.classList.contains('active') ? `${block.scrollHeight}px` : '0';
-  })
-})
-
+  });
+});
