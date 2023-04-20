4a46755f3d19cda619696968027d61e3e7764ecc
VadimKalaskin <120710280+VadimKalaskin@users.noreply.github.com>
2023-04-20 17:57:17 +0300

    2

---
+++ ER Technology/script/changeButtons.js
@@ -0,0 +1,21 @@
+const accordButtons = document.querySelectorAll('a div.btn-wrapper');
+const inlineButtons = document.querySelectorAll('button.inlineAncor');
+
+window.addEventListener('resize', () => {
+  if(window.innerWidth < 991){
+    accordButtons.forEach(item => {
+      item.style.display = 'none';
+    });
+    inlineButtons.forEach(item => {
+      item.style.display = 'inline-block';
+    })
+  } else {
+    accordButtons.forEach(item => item.style.display = 'flex');
+    inlineButtons.forEach(item => {
+      item.style.display = 'none';
+    })
+  }
+});
+
+
+
